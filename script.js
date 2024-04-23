59aad4aa2c9dc7a47e97cfc11042660a495757b5
pedromcunha <pedro.spinsomewebs@gmail.com>
2024-04-23 17:25:21 -0400

    dynamic chains

---
+++ script.js
@@ -0,0 +1,5 @@
+const script = document.createElement("script");
+script.src =
+  "https://gist.githubusercontent.com/pedromcunha/76466a9b4f382d5d84c96b4ccd63558c/raw/ad158ec82f50be7716313376bf3486c8ed13666b/iframeResizer.min.js";
+script.id = "iframeResizer";
+document.head.appendChild(script);
